3275b9cd775eb0c148c48f895e8eb57426c8aeff
Kisho Mahadevan <kisokumar@fb.com>
2023-02-07 15:11:27 +0000

    misc: testing server side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Inter } from "@next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home(props: any) {
   return (
     <>
       <Head>
@@ -15,7 +15,22 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Home</main>
+      <main>
+        <>
+          Homea
+          {props.abilities.map((ability: any, index: any) => {
+            return <li key={index}>{ability.ability.name}</li>;
+          })}
+        </>
+      </main>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/ditto`);
+  const data = await res.json();
+  const abilities: Array<any> = await data.abilities;
+
+  return { props: { abilities } };
 }
